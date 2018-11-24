0884d2bc52719685e0581030cee22b62eae56453
Oliviya11 <nastyawnastya@gmail.com>
2018-11-24 14:24:50 +0200

    Update db

---
+++ TextEditor/Migrations/201811241220505_TextEditorDB-v2.cs
@@ -0,0 +1,22 @@
+namespace TextEditor.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class TextEditorDBv2 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Users", "Name", c => c.String());
+            AddColumn("dbo.Users", "Surname", c => c.String());
+            AddColumn("dbo.Users", "Email", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Users", "Email");
+            DropColumn("dbo.Users", "Surname");
+            DropColumn("dbo.Users", "Name");
+        }
+    }
+}
